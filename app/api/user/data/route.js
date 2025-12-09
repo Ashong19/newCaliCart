ceb004ec003bd7675902dfa87aaa560ee0d4467f
Ashong <ashleyallengevera1913@gmail.com>
2025-12-09 19:33:46 +0800

    update seller added items to seller dashboard

---
+++ app/api/user/data/route.js
@@ -0,0 +1,21 @@
+import dbConnect from "@/config/db";
+import User from "@/models/Users";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request);
+
+        await dbConnect()
+        const user = await User.findById(userId);
+        if (!user) {
+            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 })
+        }
+
+        return NextResponse.json({ success: true, data: user }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message }, { status: 404 })
+
+    }
+}
